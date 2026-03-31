21c3136c58c9791946c31933bdb71293e6381896
varunraj-2005 <rajapandian1412@gmail.com>
2026-03-31 21:44:09 +0530

    Time: 1 ms (63.86%) | Memory: 44.5 MB (99.54%) - LeetSync

---
+++ 3429-special-array-i/special-array-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]%2==0){
+                if(nums[i-1]%2==0){
+                    return false;
+                }
+            }
+            else{
+                if(nums[i-1]%2!=0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
