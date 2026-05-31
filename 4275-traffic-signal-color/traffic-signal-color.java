8f630f363c4286c8cea23f57f82631b4e306594c
varunraj-2005 <rajapandian1412@gmail.com>
2026-05-31 21:26:48 +0530

    Time: 1 ms (71.30%) | Memory: 43.7 MB (5.39%) - LeetSync

---
+++ 4275-traffic-signal-color/traffic-signal-color.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String trafficSignal(int timer) {
+        if(timer==0) return "Green";
+        else if(timer==30) return "Orange";
+        else if (timer<=90 && timer>30) return "Red";
+        else return "Invalid";
+        
+   }
+}
