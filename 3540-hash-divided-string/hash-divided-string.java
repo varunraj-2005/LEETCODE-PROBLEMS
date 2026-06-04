811dda0d33dfb509cfb22e0c395d19fa45852860
varunraj-2005 <rajapandian1412@gmail.com>
2026-06-04 10:10:12 +0530

    Time: 2 ms (99.20%) | Memory: 45.8 MB (98.93%) - LeetSync

---
+++ 3540-hash-divided-string/hash-divided-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String stringHash(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i+=k){
+            int sum=0;
+            for(int j=i;j<i+k;j++){
+                sum+=s.charAt(j)-'a';
+            }
+             sb.append((char) ('a' + (sum % 26)));
+        }
+        return sb.toString();
+    }
+}
